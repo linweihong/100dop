b33bf63ef0635470fd992b0349d265769e26f3fc
Weihong <zaerkkahn+github@gmail.com>
2022-07-21 01:43:19 +0800

    import Question class and create question_bank

--- src/017.py
+++ src/017.py
@@ -1,22 +1,10 @@
 ### Quiz Game ###
 
 from res017.data import question_data
+from res017.question_model import Question
 
+question_bank = [
+    Question(question["text"], question["answer"]) for question in question_data
+]
 
-class User:
-    def __init__(self, user_id, username):
-        self.id = user_id
-        self.username = username
-        self.followers = 0
-        self.following = 0
-
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
-
-
-user_1 = User(1, "angela")
-user_2 = User(2, "jack")
-
-user_1.follow(user_2)
-print(user_1.following, user_1.followers, user_2.following, user_2.followers)
+print(question_bank)
