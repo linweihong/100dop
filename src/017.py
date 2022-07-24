72612065a76937e05b3fce49596250ae3c84dab0
Weihong <zaerkkahn+github@gmail.com>
2022-07-24 23:48:59 +0800

    implement quiz

--- src/017.py
+++ src/017.py
@@ -2,9 +2,15 @@
 
 from res017.data import question_data
 from res017.question_model import Question
+from res017.quiz_brain import QuizBrain
 
 question_bank = [
     Question(question["text"], question["answer"]) for question in question_data
 ]
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've completed the quiz.")
+print(f"Your final score was {quiz.score}/{quiz.question_number}.")
