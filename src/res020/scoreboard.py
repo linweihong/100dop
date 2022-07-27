62447cc630da16c6322586c950e1cd3a5e0033e7
Weihong <zaerkkahn+github@gmail.com>
2022-07-27 11:56:25 +0800

    implement permanent high score

--- src/res020/scoreboard.py
+++ src/res020/scoreboard.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from turtle import Turtle
 
 FONT = ("Consolas", 15, "normal")
@@ -7,7 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        self.high_score = self.get_high_score()
         self.ht()
         self.goto(0, 270)
         self.color("white")
@@ -28,9 +29,23 @@ class Scoreboard(Turtle):
     def reset_scoreboard(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            self.set_high_score()
         self.score = 0
         self.refresh_score()
 
     def game_over(self):
         self.home()
         self.write("GAME OVER", align="center", font=FONT)
+
+    def get_high_score(self):
+        path = Path(__file__).parent / "data.txt"
+        with path.open() as f:
+            if s := f.read():
+                return int(s)
+            else:
+                return 0
+
+    def set_high_score(self):
+        path = Path(__file__).parent / "data.txt"
+        with path.open("w") as f:
+            f.write(str(self.high_score))
