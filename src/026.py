4da11c9801e485285f344830790c0050bc9eee80
Weihong <zaerkkahn+github@gmail.com>
2022-07-28 22:47:56 +0800

    implement exception handling

--- src/026.py
+++ src/026.py
@@ -6,5 +6,11 @@ df = pd.read_csv("./src/res026/nato_phonetic_alphabet.csv")
 
 d = {row.letter: row.code for (index, row) in df.iterrows()}
 
-username = input("What is your name? ")
-print([d[c.upper()] for c in username if c.isalpha()])
+while True:
+    username = input("What is your name? ")
+    try:
+        print([d[c.upper()] for c in username])
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        break
