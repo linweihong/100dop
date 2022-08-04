f6e18193eb7de9953c22e6f40ef1a7ebf11e9bb4
Weihong <zaerkkahn+github@gmail.com>
2022-08-05 00:43:11 +0800

    fetch city codes

--- src/res039/flight_search.py
+++ src/res039/flight_search.py
@@ -1,3 +1,34 @@
+import json
+
+import requests
+
+from constants import endpoint
+from constants import flightsearch_key as f_apikey
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    # This class is responsible for talking to the Flight Search API.
+    def __init__(self):
+        self.dump_endpoint = endpoint + "locations/dump"
+        self.headers = {"apikey": f_apikey}
+        self.dump_payload = {
+            "locale": "en-US",
+            "location_types": "city",
+            "limit": 200,
+            "sort": "rank",
+        }
+
+    def get_dump(self):
+        r = requests.get(
+            url=self.dump_endpoint, headers=self.headers, params=self.dump_payload
+        )
+        return r.json()["locations"]
+
+    def fetch_codes(self):
+        d = self.get_dump()
+        codes = []
+        for location in d:
+            codes.append({"city": location["name"], "code": location["code"]})
+        # with open("./src/res039/test_data.json", "w") as f:
+        #     json.dump(codes, f, indent=2)
+        return codes
