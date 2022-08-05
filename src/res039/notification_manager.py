6e2285291bc379df2fcd22b8492d4cc3c56ada28
Weihong <zaerkkahn+github@gmail.com>
2022-08-06 02:37:12 +0800

    implement Telegram notifications

--- src/res039/notification_manager.py
+++ src/res039/notification_manager.py
@@ -1,3 +1,18 @@
+import asyncio
+
+import telegram
+
+from constants import CHATID_W, TELEGRAM_TOKEN
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    # This class is responsible for sending notifications with the deal flight details.
+    def __init__(self):
+        self.bot = telegram.Bot(TELEGRAM_TOKEN)
+
+    def notify(self, message):
+        asyncio.run(self.send_message(message))
+
+    async def send_message(self, message):
+        async with self.bot:
+            await self.bot.send_message(text=message, chat_id=CHATID_W)
