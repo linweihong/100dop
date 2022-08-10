f79ceb33f053ad05e1f30837d76e02e438ff47da
Weihong <zaerkkahn+github@gmail.com>
2022-08-11 01:18:37 +0800

    initial commit

---
+++ src/p051.py
@@ -0,0 +1,13 @@
+# Twitter bot
+
+
+class InternetSpeedTwitterBot:
+    def __init__(self) -> None:
+        self.down = 0
+        self.up = 0
+
+    def get_internet_speed(self):
+        pass
+
+    def tweet_at_provider(self):
+        pass
