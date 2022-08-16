8da4411fac1789b5a4531b4d4d6f3eb0b1fce919
Weihong <zaerkkahn+github@gmail.com>
2022-08-16 23:31:02 +0800

    implement lesson

--- src/res054/hello.py
+++ src/res054/hello.py
@@ -3,6 +3,33 @@ from flask import Flask
 app = Flask(__name__)
 
 
+def strong(func):
+    def wrapper():
+        return f"<strong>{func()}</strong>"
+
+    return wrapper
+
+def em(func):
+    def wrapper():
+        return f"<em>{func()}</em>"
+
+    return wrapper
+
+
 @app.route("/")
 def hello_world():
     return "Hello, World!"
+
+
+@app.route("/bye")
+def bye():
+    return "Bye."
+
+
+@app.route("/username/<name>")
+def greet(name):
+    return f"Hello {name}."
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
